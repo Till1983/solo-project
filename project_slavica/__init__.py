e247d4baa95a52c987e581d95d1bcd7ff88b61e0
Till1983 <42179755+Till1983@users.noreply.github.com>
2019-04-28 23:33:29 +0200

    login and register routes updated

---
+++ project_slavica/__init__.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, url_for, request, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '4d625c178caa104cc9a8a92c68c1120b'
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+db = SQLAlchemy(app)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About the Project')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'You have successfully created your account, {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data =='somepassword':
+            flash('You are now logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login failed! Please check username and/or password.', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
